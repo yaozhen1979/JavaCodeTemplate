473b0fb3f04103a31f84cf63a70bace91fc1cfd0
yaozhen <yaozhen1979@gmail.com>
2014-02-02 13:59:44 +0800

    java basic template initial commit

---
+++ src/main/java/com/example/Greeter.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class Greeter {
+	public String sayHello() {
+		return "Hello world!";
+	}
+}
---
+++ src/main/java/com/example/Test.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.joda.time.LocalTime;
+
+public class Test {
+
+	public static void main(String[] args) {
+		LocalTime currentTime = new LocalTime();
+	    System.out.println("The current local time is: " + currentTime);
+
+	    Greeter greeter = new Greeter();
+	    System.out.println(greeter.sayHello());
+	}
+
+}
